44977b78de8107aee2adade4706cdb53e8f8c417
SidimaD <43736678+Sidimadtv@users.noreply.github.com>
2023-05-26 05:04:18 +0200

    Delete m3u directory

--- m3u/js/bootstrap.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-	"use strict";
-
-		$('ul.navbar-nav li a').on('click', function() {
-			$('ul.navbar-nav li').removeClass('active');
-			$(this).parents('li').addClass('active');
-		});
-		
-})(jQuery);
